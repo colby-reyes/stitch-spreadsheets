353daed14379e829155860cfffb93c2011d820df
colby-reyes <colby.n.reyes@gmail.com>
2024-05-13 18:51:57 +0000

    Update title

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,14 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+st.set_page_config(
+    page_title="Spreadsheet Stitcher",
+    page_icon="https://www.logolynx.com/images/logolynx/4f/4f42c461be2388aca949521bbb6a64f1.gif",
+    layout="wide",
+)
+
+st.title(body=":sewing_needle: :thread: Spreadsheet Stitcher")
+
+
+
 
