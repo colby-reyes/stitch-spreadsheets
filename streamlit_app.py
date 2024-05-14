0bc502e24156324577da4a9773c008e397c42b1d
colby-reyes <colby.n.reyes@gmail.com>
2024-05-14 22:07:36 +0000

    Move notifications to st.status element; add reset and download buttons

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import time
+# import time
 
 st.set_page_config(
     page_title="Spreadsheet Stitcher",
@@ -14,6 +14,7 @@ st.set_page_config(
 
 
 def stitch_spreadsheets(file_list:list):
+    status_bar = st.status("Loading spreadsheets ... ",state="running",expanded=True)
     
     df_list = []
     success_list = []
@@ -22,15 +23,21 @@ def stitch_spreadsheets(file_list:list):
             try:
                 df_list.append(pd.read_excel(i))
                 success_list.append(i.name)
+                status_bar.write(f" * {i.name}")
             except Exception as excel_error:
-                st.toast(f"Skipping file: {i.name}\n\n {excel_error}")
+                # st.toast(f"Skipping file: {i.name}\n\n {excel_error}")
+                status_bar.write(f" * SKIPPED: {i.name} ({excel_error})")
         else:
             try:
                 df_list.append(pd.read_csv(i))
                 success_list.append(i.name)
+                status_bar.write(f" * {i.name}")
             except Exception as csv_error:
-                st.toast(f"Skipping file {i.name}:\n\n {csv_error}")
+                # st.toast(f"Skipping file {i.name}:\n\n {csv_error}")
+                status_bar.write(f" * SKIPPED: {i.name} ({csv_error})")
     try:
+        status_bar.update(label="Stitching spreadsheets ...",state="running")
+        status_bar.write("Combining spreadsheets...")
         final_df = pd.concat(df_list)
         pre_dedup_len = len(final_df)
         if st.session_state.deduplicate_tf is True:
@@ -46,16 +53,27 @@ def stitch_spreadsheets(file_list:list):
 
     success_msg = '\n\n * ' + '\n\n * '.join(success_list)
     st.session_state.button_state = "secondary"
+    status_bar.write("Done!")
+    status_bar.update(label=":thread: Spreadsheets stitched!",state="complete",expanded=False)
 
-    notification_container = st.container().empty()
-    with notification_container:
-        st.success(f"File(s) read and stitched successfully: {success_msg}")
-        time.sleep(1)
-        notification_container.empty()
+    # notification_container = st.container().empty()
+    # with notification_container:
+    #     st.success(f"File(s) read and stitched successfully: {success_msg}")
+    #     time.sleep(1)
+    #     notification_container.empty()
+    #     if notification_container.button("Reset",type="primary",use_container_width=True):
+    #         st.rerun()
+        
     st.toast("Done!")
 
 def set_button_primary():
     st.session_state.button_state = "primary"
+
+@st.cache_data
+def convert_df(df):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    return df.to_csv().encode("utf-8")
+
     
 
 def main():
@@ -81,11 +99,22 @@ def main():
         with c1:
             st.session_state. deduplicate_tf = st.checkbox("Deduplicate?",value=False,help="Check this box to remove duplicates when stitching the spreadsheets together. Only do this if you are sure that duplciate lines should be removed!")
         with c2:
-            st.button("Stitch Spreadsheets", on_click=stitch_spreadsheets, args=[st.session_state.uploaded_files], type=st.session_state.button_state)
+            st.button("Stitch Spreadsheets", on_click=stitch_spreadsheets, args=[st.session_state.uploaded_files], type=st.session_state.button_state,use_container_width=True)
         # st.write(st.session_state.uploaded_files)
     
     if st.session_state.final_df is not None:
         st.dataframe(st.session_state.final_df)
+        csv = convert_df(st.session_state.final_df)
+
+        st.download_button(
+            label="Download data as CSV",
+            data=csv,
+            file_name="large_df.csv",
+            mime="text/csv",
+            type="primary",
+            use_container_width=True,
+        )
+
 
 
 if __name__ == "__main__":
